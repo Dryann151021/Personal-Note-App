72355084f5eb5a745d494a883471bf0bea0bf890
Adriyan <adrianadrian.aa613@gmail.com>
2025-09-26 22:42:44 +0700

    Perbaikan styling NoteItem & link di header

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -6,13 +6,13 @@ import parser from 'html-react-parser';
 
 function NoteItem({ id, title, createdAt, body, }) {
   return(
-    <article className='note-item'>
-      <h3 className='note-item__title'>
-        <Link to={`/notes/${id}`}>{title}</Link>
-      </h3>
-      <p className='note-item__createdAt'>{showFormattedDate(createdAt)}</p>
-      <p className='note-item__body'>{parser(body)}</p>
-    </article>
+    <Link to={`/notes/${id}`} className='note-item'>
+      <article>
+          <h3 className='note-item__title'>{title}</h3>
+          <p className='note-item__createdAt'>{showFormattedDate(createdAt)}</p>
+          <p className='note-item__body'>{parser(body)}</p>
+      </article>
+    </Link>
   );
 };
 
